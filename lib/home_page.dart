49fe180660f7b5f940a95108294a527e3c1216f9
ialria <your_real_email@gmail.com>
2026-04-24 18:20:55 +0500

    step-three

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:first/utility_pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' show log;
+
+enum MenuAction { logout }
+
+Future<bool> showLogoutDialog(BuildContext context) {
+  return showDialog<bool>(context: context, builder: (context) {
+    return AlertDialog(
+      title: Text("Log out"),
+      content: Text("Are you sure you want to Log out?"),
+      actions: [
+        OutlinedButton(onPressed: (){
+          Navigator.of(context).pop(false);
+        }, child: Text("Cancel")),
+        FilledButton(onPressed: (){
+          Navigator.of(context).pop(true);
+
+        }, child: Text("Logout"))
+      ],
+    );
+  },).then((value) => value ?? false,);
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,8 +31,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-
   @override
   void initState() {
     // TODO: implement initState
@@ -19,12 +39,32 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-    appBar: AppBar(
-    title: Text("Home"),
-    ),
-  body: Text("Home Page"),
+      appBar: AppBar(
+        title: Text("Home"),
+        actions: [
+          PopupMenuButton(
+            onSelected: (value)async {
+              final shouldLogout=await showLogoutDialog(context);
+              if(shouldLogout){
+                FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder:(_)=>LoginPage()),(route)=>false);
+              }
+              log(shouldLogout.toString());
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  value: MenuAction.logout,
+                  child: Text("Log out"),
+                ),
+                PopupMenuItem(child: Text("Rate")),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: Text("Home Page"),
     );
   }
 }
